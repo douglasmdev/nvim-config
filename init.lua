a3b8c46d05a1012e3cd22be91589b083b1f5235b
Douglas Montes <douglasmontes.dev@gmail.com>
2026-01-23 19:39:21 -0300

    Add save and quit keymaps

--- init.lua
+++ init.lua
@@ -37,15 +37,15 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "Save file in normal mode" })
+vim.keymap.set("i", "<C-s>", "<esc>:w<cr>", { desc = "Save file in insert mode" })
+vim.keymap.set("n", "<C-q>", ":q<cr>", { desc = "Quit normal mode" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
