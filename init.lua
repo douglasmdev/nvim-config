996ebf4e9579c59446a422dbb7b868f13209190a
Douglas Montes <douglasmontes.dev@gmail.com>
2026-01-25 17:28:49 -0300

    fix statusline contrast

--- init.lua
+++ init.lua
@@ -497,6 +497,16 @@ require("lazy").setup({
 			statusline.section_location = function()
 				return "%2l:%-2v"
 			end
+			local function set_statusline_hl()
+				vim.api.nvim_set_hl(0, "MiniStatuslineFilename", { fg = "#eee8d5", bg = "#073642", bold = false })
+				vim.api.nvim_set_hl(0, "MiniStatuslineDevinfo", { fg = "#93a1a1", bg = "#073642" })
+				vim.api.nvim_set_hl(0, "MiniStatuslineFileinfo", { fg = "#93a1a1", bg = "#073642" })
+			end
+			set_statusline_hl()
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				group = vim.api.nvim_create_augroup("custom-mini-statusline-hl", { clear = true }),
+				callback = set_statusline_hl,
+			})
 		end,
 	},
 	{
